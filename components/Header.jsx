5ed2c406c4243865a1e4aec51fc7bcf822378f35
bhrtsnegi <1bhrtsnegi@gmail.com>
2025-01-30 10:49:30 +0530

    add dark mode functionality

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Header = () => {
+const Header = (isDarkMode) => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
         <div>
@@ -21,10 +21,10 @@ const Header = () => {
             officia deleniti impedit enim et. Reiciendis, dignissimos aspernatur!
         </p>
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-            <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
+            <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>
                 Contact Me <Image src={assets.right_arrow_white} alt='' className='w-4' /> 
             </a>
-            <a href="./sample-resume.pdf" download className='px-10 py-3 rounded-full border-gray-500 flex items-center gap-2'>
+            <a href="./sample-resume.pdf" download className='px-10 py-3 rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
                 My Resume <Image src={assets.download_icon} alt='' className='w-4' /> 
             </a>
         </div>
